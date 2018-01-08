eb9e32731283b8d1c8695705f76e92f42601672c
Philip Tran <ptdevel@gmail.com>
2018-01-08 10:07:38 -0800

    Add settings for web development

---
+++ config/web_settings.el
@@ -0,0 +1,26 @@
+;; web_settings.el
+;;
+;; Author:  Philip Tran
+;; URL:     https://github.com/ptran516/.emacs.d
+
+(use-package web-mode
+  :ensure t
+  :config
+  ;; Indentation preferences
+  (defun my/web-mode-hook ()
+    (setq web-mode-markup-indent-offset 2)
+    (setq web-mode-css-indent-offset 2)
+    (setq web-mode-code-indent-offset 2))
+  (add-hook 'web-mode-hook 'my/web-mode-hook)
+
+  ;; Associate web files to this mode
+  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
+
+  (setq web-mode-engines-alist
+        '(("jinja" . "\\.html?\\'"))))

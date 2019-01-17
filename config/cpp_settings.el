0122853df12b6e9e90ecf634c390bdc186508f0d
Philip Tran <dev@philtran.net>
2019-01-16 21:31:44 -0800

    Add custom C++ CMake build function

--- config/cpp_settings.el
+++ config/cpp_settings.el
@@ -12,13 +12,41 @@
   (c-set-offset 'arglist-intro '+)
   (c-set-offset 'arglist-close '0)
   (c-set-offset 'inline-open '0))
-(add-hook 'c-mode-common-hook #'my/c-indent-hook)
+(add-hook 'c-mode-hook #'my/c-indent-hook)
+(add-hook 'c++-mode-hook #'my/c-indent-hook)
 
 ;; Display line numbers
-(add-hook 'c-mode-common-hook #'linum-mode)
+(add-hook 'c-mode-hook #'linum-mode)
+(add-hook 'c++-mode-hook #'linum-mode)
 
 ;; Open .h files in c++ mode
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+
+(if (buffer-file-name)
+    (file-name-directory buffer-file-name)
+  (print "Current buffer is not a file"))
+
+;; Custom cmake build function (requires projectile)
+(defun my/build-cmake-project (&optional cmake-args)
+  "Build CMake project for current buffer"
+  (interactive)
+  (if (buffer-file-name)
+      (setq buffer-project-root (projectile-project-root buffer-file-name))
+    (error "my/build-cmake-project error: Current buffer is not a file"))
+  (setq cmakelists-file (concat buffer-project-root "CMakeLists.txt"))
+  (unless (file-exists-p cmakelists-file)
+    (error "my/build-cmake-project error: %s" (concat buffer-project-root " does not have a CMakeLists.txt file")))
+  (setq build-directory (concat buffer-project-root "build"))
+  (unless (file-exists-p build-directory)
+    (make-directory build-directory))
+  (setq cmake-command (concat "cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON " cmake-args))
+  (if (string-equal system-type "windows-nt")
+      (setq build-command "cmake --build . --config Release")
+    (setq build-command "make -j$(nproc)"))
+  (async-shell-command (concat "cd " build-directory "; " cmake-command "; " build-command) "*my/cmake-build*"))
+
+;; Bind build function to C-c m
+(add-hook 'c++-mode-hook (lambda () (define-key c++-mode-map (kbd "C-c m") #'my/build-cmake-project)))
 
 ;; Irony-Mode
 ;; ---------------------------------------------------------------------------
@@ -64,5 +92,6 @@
   :no-require t
   :ensure t
   :config
-  (add-hook 'c-mode-common-hook #'flycheck-mode)
+  (add-hook 'c-mode-hook #'flycheck-mode)
+  (add-hook 'c++-mode-hook #'flycheck-mode)
   (add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
